f54b611c83bb3caa5def8f42f80ba0864982e82a
Alvaro Angel <86565212+bitnibelun@users.noreply.github.com>
2023-01-15 12:41:56 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,43 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify( books, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn]
+  res.send(book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const authorName = req.params.author;
+
+  for(let key in books) {
+    if (books[key].author.toLowerCase() === authorName.toLowerCase()) {
+      res.send(books[key]);
+    }
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  for(let key in books) {
+    if (books[key].title.toLowerCase() === title.toLowerCase()) {
+      res.send(books[key]);
+    }
+  }
 });
 
 //  Get book review
